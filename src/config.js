4ceee2e85de201ca2a91713d5cd203e954eb5e59
Kier Borromeo <seraphipod@gmail.com>
2018-07-07 17:43:12 +0800

    Update description

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export default {
   title: 'Tech Brew',
   keyline: 'We turn ideas into life',
-  description: `We are a digital agency focused on satisfying our client's customers with good user experience and good technology choices.`,
+  description: `We are a digital agency focused on satisfying our client's customers through quality software, emphasizing on enjoyable user experiences.`,
   email: 'hey@techbrewlabs.com',
   keywords: [
     'laravel',
